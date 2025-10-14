188efc3ae6d0fe253a51c53616525cbcbe271464
Baki-c <anwarjelloul@gmail.com>
2025-10-14 21:29:25 +0100

    intial commit

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
+const CACHE_NAME = "coffee-survey-form-cache-v1";
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("coffee-survey-v1").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         "/coffee-survey-form/",
         "/coffee-survey-form/index.html",
@@ -11,10 +13,14 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
   );
 });
