dd0c6a3cf3dabecfa498f7bdf5095a28ce9305f8
Baki-c <anwarjelloul@gmail.com>
2025-10-14 18:59:01 +0100

    Describe your update

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("coffee-survey-cache").then(cache => {
+    caches.open("coffee-survey-cache").then((cache) => {
       return cache.addAll([
-        "/Customer-Satisfaction-Survey/",
-        "/Customer-Satisfaction-Survey/index.html",
-        "/Customer-Satisfaction-Survey/styles.css",
-        "/Customer-Satisfaction-Survey/scripts.js"
+        "/coffee-survey-form/",
+        "/coffee-survey-form/index.html",
+        "/coffee-survey-form/styles.css",
+        "/coffee-survey-form/scripts.js",
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
